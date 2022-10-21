648d05651c672fd112a9855128986ba24c4470f6
Louis Collard <louiscollard2@gmail.com>
2022-10-21 16:45:03 +0200

    Add second route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+let pokemons = require("./mock-pokemon");
 
 const app = express();
 const port = 3000;
@@ -8,8 +9,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/pokemons/:id", (req, res) => {
-	const id = req.params.id;
-	res.send(`Vous avez demandé le pokémon n°${id}`);
+	const id = parseInt(req.params.id);
+	const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+	res.send(`Vous avez demandé le pokémon ${pokemon.name}`);
+});
+
+app.get("/api/pokemons", (req, res) => {
+	res.send(`Il y a ${pokemons.length} pokémons dans le pokédex`);
 });
 
 app.listen(port, console.log(`Notre application est démarée sur http://localhost:${port}`));
