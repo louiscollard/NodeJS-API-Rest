00ff1cbe3e909ab99feebd212dd959ba1dd69fad
Louis Collard <louiscollard2@gmail.com>
2022-10-21 16:05:18 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Hello Express!");
+});
+
+app.get("/api/pokemons/:id", (req, res) => {
+	const id = req.params.id;
+	res.send(`Vous avez demandé le pokémon n°${id}`);
+});
+
+app.listen(port, console.log(`Notre application est démarée sur http://localhost:${port}`));
