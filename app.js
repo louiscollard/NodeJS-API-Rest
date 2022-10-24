0ac1ec2cfec9ffc84ecfb99ce92ada6b70e25486
Louis Collard <louiscollard2@gmail.com>
2022-10-24 09:46:39 +0200

    Add helpers

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { success } = require("./helper");
 let pokemons = require("./mock-pokemon");
 
 const app = express();
@@ -11,11 +12,13 @@ app.get("/", (req, res) => {
 app.get("/api/pokemons/:id", (req, res) => {
 	const id = parseInt(req.params.id);
 	const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-	res.send(`Vous avez demandé le pokémon ${pokemon.name}`);
+	const message = "Un pokémon a bien été trouvé.";
+	res.json(success(message, pokemon));
 });
 
 app.get("/api/pokemons", (req, res) => {
-	res.send(`Il y a ${pokemons.length} pokémons dans le pokédex`);
+	const message = "La liste de pokémons a bien été récupérée.";
+	res.json(success(message, pokemons));
 });
 
 app.listen(port, console.log(`Notre application est démarée sur http://localhost:${port}`));
