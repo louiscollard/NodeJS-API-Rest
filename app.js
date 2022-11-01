996a2e0c5d0650d90dd677a04774565771c4589e
Louis Collard <louiscollard2@gmail.com>
2022-11-01 12:59:57 +0100

    Add routes

--- app.js
+++ app.js
@@ -13,4 +13,10 @@ app
 
 sequelize.initDb();
 
+require("./src/routes/findAllPokemons")(app);
+require("./src/routes/findPokemonByPk")(app);
+require("./src/routes/createPokemon")(app);
+require("./src/routes/updatePokemon")(app);
+require("./src/routes/deletePokemon")(app);
+
 app.listen(port, console.log(`Notre application est démarée sur http://localhost:${port}`));
