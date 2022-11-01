b24fec90b44b3d45b0b2d80172a24212fac3fd30
Louis Collard <louiscollard2@gmail.com>
2022-11-01 12:19:54 +0100

    Refactoring

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const favicon = require("serve-favicon");
-const { success, getUniqueId } = require("./helper");
-let pokemons = require("./mock-pokemon");
+const sequelize = require("./src/db/sequelize");
 
 const app = express();
 const port = 3000;
@@ -12,46 +11,6 @@ app
 	.use(favicon(__dirname + "/favicon.ico"))
 	.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-	res.send("Hello Express!");
-});
-
-app.get("/api/pokemons/:id", (req, res) => {
-	const id = parseInt(req.params.id);
-	const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-	const message = "Un pokémon a bien été trouvé.";
-	res.json(success(message, pokemon));
-});
-
-app.get("/api/pokemons", (req, res) => {
-	const message = "La liste de pokémons a bien été récupérée.";
-	res.json(success(message, pokemons));
-});
-
-app.post("/api/pokemons", (req, res) => {
-	const id = getUniqueId(pokemons);
-	const pokemonCreated = { ...req.body, ...{ id: id, created: new Date() } };
-	pokemons.push(pokemonCreated);
-	const message = `Le pokémon ${pokemonCreated.name} a bien été crée.`;
-	res.json(success(message, pokemonCreated));
-});
-
-app.put("/api/pokemons/:id", (req, res) => {
-	const id = parseInt(req.params.id);
-	const pokemonUptaded = { ...req.body, id: id };
-	pokemons = pokemons.map((pokemon) => {
-		return pokemon.id === id ? pokemonUptaded : pokemon;
-	});
-	const message = `Le pokémon ${pokemonUptaded.name} a bien été modifié.`;
-	res.json(success(message, pokemonUptaded));
-});
-
-app.delete("/api/pokemons/:id", (req, res) => {
-	const id = parseInt(req.params.id);
-	const pokemonDeleted = pokemons.find((pokemon) => pokemon.id === id);
-	pokemons = pokemons.filter((pokemon) => pokemon.id !== id);
-	const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé.`;
-	res.json(success(message, pokemonDeleted));
-});
+sequelize.initDb();
 
 app.listen(port, console.log(`Notre application est démarée sur http://localhost:${port}`));
