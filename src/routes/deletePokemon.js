5890119816a8df89d4b919a6f7fbfc4fcb61fc32
Louis Collard <louiscollard2@gmail.com>
2022-11-03 12:10:09 +0100

    Add status

--- src/routes/deletePokemon.js
+++ src/routes/deletePokemon.js
@@ -2,14 +2,23 @@ const { Pokemon } = require("../db/sequelize");
 
 module.exports = (app) => {
 	app.delete("/api/pokemons/:id", (req, res) => {
-		Pokemon.findByPk(req.params.id).then((pokemon) => {
-			const pokemonDeleted = pokemon;
-			Pokemon.destroy({
-				where: { id: pokemon.id },
-			}).then((_) => {
-				const message = `Le pokémon avec l'identifiant n°${pokemonDeleted.id} a bien été supprimé.`;
-				res.json({ message, data: pokemonDeleted });
+		Pokemon.findByPk(req.params.id)
+			.then((pokemon) => {
+				if (pokemon === null) {
+					const message = "Le pokémon n'existe pas. Réesayez avec un autre identifiant.";
+					res.status(404).json({ message });
+				}
+				const pokemonDeleted = pokemon;
+				return Pokemon.destroy({
+					where: { id: pokemon.id },
+				}).then((_) => {
+					const message = `Le pokémon avec l'identifiant n°${pokemonDeleted.id} a bien été supprimé.`;
+					res.json({ message, data: pokemonDeleted });
+				});
+			})
+			.catch((error) => {
+				const message = "Le pokémon n'a pas pu être supprimé. Réesayez dans quelques instants.";
+				res.status(500).json({ message, data: error });
 			});
-		});
 	});
 };
--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -5,11 +5,20 @@ module.exports = (app) => {
 		const id = req.params.id;
 		Pokemon.update(req.body, {
 			where: { id: id },
-		}).then((_) => {
-			Pokemon.findByPk(id).then((pokemon) => {
-				const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
-				res.json({ message, data: pokemon });
+		})
+			.then((_) => {
+				return Pokemon.findByPk(id).then((pokemon) => {
+					if (pokemon === null) {
+						const message = "Le pokémon n'existe pas. Réesayez avec un autre identifiant.";
+						res.status(404).json({ message });
+					}
+					const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
+					res.json({ message, data: pokemon });
+				});
+			})
+			.catch((error) => {
+				const message = "Le pokémon n'a pas pu être modifiée. Réesayez dans quelques instants.";
+				res.status(500).json({ message, data: error });
 			});
-		});
 	});
 };
