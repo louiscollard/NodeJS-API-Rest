fdecd34361d62d9a211b58a85f9e5e842c999131
Louis Collard <louiscollard2@gmail.com>
2022-11-03 15:31:55 +0100

    Add validators

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,3 +1,5 @@
+const validTypes = ["Plante", "Poison", "Feu", "Eau", "Insecte", "Vol", "Normal", "Electrik", "Fée"];
+
 module.exports = (sequelize, DataTypes) => {
 	return sequelize.define(
 		"Pokemon",
@@ -10,18 +12,51 @@ module.exports = (sequelize, DataTypes) => {
 			name: {
 				type: DataTypes.STRING,
 				allowNull: false,
+				unique: { msg: "Le nom est déjà pris." },
+				validate: {
+					notEmpty: { msg: "Le nom ne peut pas être vide" },
+					notNull: { msg: "Le nom est une propriété requise." },
+				},
 			},
 			hp: {
 				type: DataTypes.INTEGER,
 				allowNull: false,
+				validate: {
+					isInt: { msg: "Utitisez uniquement des nombres entiers pour les points de vie." },
+					notNull: { msg: "Les points de vie sont une propriété requise." },
+					min: {
+						args: [0],
+						msg: "Les points de vie doivent être supérieur à 0",
+					},
+					max: {
+						args: [999],
+						msg: "Les points de vie doivent être inférieur ou égale à 99",
+					},
+				},
 			},
 			cp: {
 				type: DataTypes.INTEGER,
 				allowNull: false,
+				validate: {
+					isInt: { msg: "Utitisez uniquement des nombres entiers pour les points de dégâts." },
+					notNull: { msg: "Les points dedégâts sont une propriété requise." },
+				},
+				min: {
+					args: [0],
+					msg: "Les points de dégâts doivent être supérieur à 0",
+				},
+				max: {
+					args: [99],
+					msg: "Les points de dégâts doivent être inférieur ou égale à 99",
+				},
 			},
 			picture: {
 				type: DataTypes.STRING,
 				allowNull: false,
+				validate: {
+					isUrl: { msg: "Utitisez uniquement une URL valide pour l'image." },
+					notNull: { msg: "L'image est une propriété requise." },
+				},
 			},
 			types: {
 				type: DataTypes.STRING,
@@ -31,6 +66,21 @@ module.exports = (sequelize, DataTypes) => {
 				},
 				set(types) {
 					this.setDataValue("types", types.join());
+				},
+				validate: {
+					isTypesValid(value) {
+						if (!value) {
+							throw new Error("Un pokémon doit au moins avoir un type.");
+						}
+						if (value.split(",").length > 3) {
+							throw new Error("Un pokémon ne peut pas avoir plus de trois types.");
+						}
+						value.split(",").forEach((type) => {
+							if (!validTypes.includes(type)) {
+								throw new Error(`Le type d'un pokémon doit appartenir à la liste suivante : ${validTypes}`);
+							}
+						});
+					},
 				},
 			},
 		},
